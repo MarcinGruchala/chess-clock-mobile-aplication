73c53a025ec44d6a7f38acc8b709100a3acfb6d1
Marcin Gruchała <margruchala@gmail.com>
2021-03-25 19:30:10 +0100

    Created class Time. -getString method is working

--- app/src/main/java/com/example/chess_clock/MainActivity.kt
+++ app/src/main/java/com/example/chess_clock/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.chess_clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.example.chess_clock.databinding.ActivityMainBinding
+import java.time.Clock
+import java.time.Instant
+import java.util.concurrent.TimeUnit
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val time = System.currentTimeMillis()
+        var startTime = 0L
+        var stopTime = 0L
+        var moveDuration = 0L
+        var timer1 = Time(0,15,5,0)
+        var timer2 = Time(0,15,5,0)
+        binding.btnTime1.text = "${timer1.getString(Time.MINUTES)}:${timer1.getString(Time.SECONDS)}"
+        binding.btnTime2.text = "${timer2.getString(Time.MINUTES)}:${timer2.getString(Time.SECONDS)}"
+        var tmpTime = 0L
+
+        binding.btnTime1.setOnClickListener {
+//            startTime = System.currentTimeMillis()
+
+
+
+        }
+
+        binding.btnTime2.setOnClickListener {
+//            stopTime = System.currentTimeMillis()
+//            moveDuration = startTime - stopTime
+//            timer2.allToMilliSeconds()
+//            tmpTime = timer2.milliSeconds - moveDuration
+//            timer2 = toTime(tmpTime)
+//            binding.btnTime2.text = "${timer2.minutes}:${timer2.seconds}"
+        }
     }
+
+//    private  fun toTime(measuredTime: Long): Time{
+//        var seconds = measuredTime / 1000
+//        var milliSeconds = measuredTime - seconds * 1000
+//        var minutes = seconds / 60
+//        seconds -= minutes * 60
+//        var hours = minutes / 60
+//        minutes -= hours * 60
+//        return Time(hours,minutes,seconds,milliSeconds)
+//    }
 }
---
+++ app/src/main/java/com/example/chess_clock/Time.kt
@@ -0,0 +1,67 @@
+package com.example.chess_clock
+
+import android.icu.util.TimeUnit
+
+class Time(
+    var hours: Long,
+    var minutes: Long,
+    var seconds: Long,
+    var milliSeconds: Long
+) {
+
+    companion object{
+        val HOURS = 1
+        val MINUTES = 2
+        val SECONDS = 3
+        val MILLISECONDS = 4
+    }
+
+
+    fun getString(timeUnit: Int): String{
+        when(timeUnit){
+            HOURS -> {
+                if(hours>9)
+                    return "${hours}"
+                else
+                    return "0$hours"
+            }
+            MINUTES -> {
+                if(minutes>9)
+                    return "$minutes"
+                else
+                    return "0$minutes"
+            }
+            SECONDS -> {
+                if(seconds>9)
+                    return "$seconds"
+                else
+                    return "0$seconds"
+            }
+            MILLISECONDS -> {
+                if(milliSeconds/10>9)
+                    return "${milliSeconds/10}"
+                else
+                    return "0${milliSeconds/10}"
+            }
+            else -> return ""
+        }
+    }
+
+    fun allToMilliSeconds(){
+        minutes += hours*60
+        hours = 0
+        seconds += minutes*60
+        minutes = 0
+        milliSeconds += seconds * 1000
+        seconds = 0
+    }
+
+//    fun allToOneUnit(timeUnit: TimeUnit){
+//
+//    }
+
+    override fun toString(): String {
+        return "$hours:$minutes:$seconds:$milliSeconds"
+    }
+
+}
