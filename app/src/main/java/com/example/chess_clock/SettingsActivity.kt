4dccc87e84586ae6e6a7742ab7a740fc01ca7b0c
Marcin Gruchała <margruchala@gmail.com>
2021-03-31 16:49:09 +0200

    Updated settings button and toolbar designs

--- app/src/main/java/com/example/chess_clock/SettingsActivity.kt
+++ app/src/main/java/com/example/chess_clock/SettingsActivity.kt
@@ -1,11 +1,31 @@
 package com.example.chess_clock
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.settings_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.itemGoBack -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
