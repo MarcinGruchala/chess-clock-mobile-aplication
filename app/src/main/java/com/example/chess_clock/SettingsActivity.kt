121dcdb3924ef95913ec5fbe9248f5d7cb0fd86c
Marcin Gruchała <margruchala@gmail.com>
2021-04-12 15:24:28 +0200

    onSharedPreferenceChanged listener set up

--- app/src/main/java/com/example/chess_clock/SettingsActivity.kt
+++ app/src/main/java/com/example/chess_clock/SettingsActivity.kt
@@ -7,26 +7,27 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.CheckBoxPreference
 import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 
- class SettingsActivity : AppCompatActivity(){
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class SettingsActivity : AppCompatActivity() {
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         setSupportActionBar(findViewById(R.id.settings_toolbar))
         supportFragmentManager.beginTransaction().replace(R.id.fragmentSettings, SettingsFragment()).commit()
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+     }
+
+     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.settings_menu, menu)
         return true
-    }
+     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.itemGoBack -> {
                 updateSettings()
@@ -35,15 +36,17 @@ import androidx.preference.PreferenceManager
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
+     }
 
-    private fun updateSettings(){
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        Settings.oneMin = sharedPreferences.getBoolean("oneMin",true)
-        Settings.twoMin = sharedPreferences.getBoolean("twoMin", false)
-        Settings.threeMin = sharedPreferences.getBoolean("threeMin", false)
-        Settings.fiveMin = sharedPreferences.getBoolean("fiveMin", false)
-        Settings.tenMin = sharedPreferences.getBoolean("tenMin",false)
-        Settings.fifteenMin = sharedPreferences.getBoolean("fifTeen", false)
-    }
+
+
+     private fun updateSettings(){
+         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+         Settings.oneMin = sharedPreferences.getBoolean("oneMin", true)
+         Settings.twoMin = sharedPreferences.getBoolean("twoMin", false)
+         Settings.threeMin = sharedPreferences.getBoolean("threeMin", false)
+         Settings.fiveMin = sharedPreferences.getBoolean("fiveMin", false)
+         Settings.tenMin = sharedPreferences.getBoolean("tenMin", false)
+         Settings.fifteenMin = sharedPreferences.getBoolean("fifTeen", false)
+     }
 }
--- app/src/main/java/com/example/chess_clock/SettingsFragment.kt
+++ app/src/main/java/com/example/chess_clock/SettingsFragment.kt
@@ -1,14 +1,28 @@
 package com.example.chess_clock
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
-import androidx.preference.SwitchPreferenceCompat
+import androidx.preference.*
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if(key=="oneMin"){
+            Log.d("Settings","minOne Change")
+        }
     }
 }
